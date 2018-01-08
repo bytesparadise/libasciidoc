8457fa5b7e902f8a6e7d5181d63a4f08943d52eb
Xavier Coulon <xcoulon@redhat.com>
2018-01-08 23:06:03 +0100

    feat(renderer): render external links without description (#50)
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- renderer/html5/external_link.go
+++ renderer/html5/external_link.go
@@ -14,12 +14,26 @@ var externalLinkTmpl *template.Template
 
 // initializes the templates
 func init() {
-	externalLinkTmpl = newHTMLTemplate("external link", `<a href="{{ .URL }}">{{ .Text }}</a>`)
+	externalLinkTmpl = newHTMLTemplate("external link", `<a href="{{ .URL }}"{{if .Class}} class="{{ .Class }}"{{ end }}>{{ .Text }}</a>`)
 }
 
 func renderExternalLink(ctx *renderer.Context, l *types.ExternalLink) ([]byte, error) {
 	result := bytes.NewBuffer(nil)
-	err := externalLinkTmpl.Execute(result, l)
+	text := l.Text
+	class := ""
+	if l.Text == "" {
+		text = l.URL
+		class = "bare"
+	}
+	err := externalLinkTmpl.Execute(result, struct {
+		URL   string
+		Text  string
+		Class string
+	}{
+		URL:   l.URL,
+		Text:  text,
+		Class: class,
+	})
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to render external link")
 	}
--- renderer/html5/external_link_test.go
+++ renderer/html5/external_link_test.go
@@ -3,6 +3,7 @@ package html5_test
 import . "github.com/onsi/ginkgo"
 
 var _ = Describe("Links", func() {
+
 	Context("External links", func() {
 
 		It("External link alone", func() {
@@ -10,6 +11,15 @@ var _ = Describe("Links", func() {
 			actualContent := "https://foo.com[the website]"
 			expected := `<div class="paragraph">
 <p><a href="https://foo.com">the website</a></p>
+</div>`
+			verify(GinkgoT(), expected, actualContent)
+		})
+
+		It("External link without description", func() {
+
+			actualContent := "https://foo.com[]"
+			expected := `<div class="paragraph">
+<p><a href="https://foo.com" class="bare">https://foo.com</a></p>
 </div>`
 			verify(GinkgoT(), expected, actualContent)
 		})
