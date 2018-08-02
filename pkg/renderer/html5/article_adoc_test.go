29a89852dd4d13c093f551dc7490155fb976beb4
Xavier Coulon <xcoulon@redhat.com>
2018-08-02 23:51:23 +0200

    fix(parser): fix parsing issue when processing 'article.adoc' (#164)
    main issue was fixed in 9f1e39437a2a6b61c19eb3f4dde12fc4ad4cae25
    but this commit adds a test to ensure that the `article.adoc` file
    will remain parse-able.
    
    fixes #153
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ pkg/renderer/html5/article_adoc_test.go
@@ -0,0 +1,34 @@
+package html5_test
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"os"
+
+	"github.com/bytesparadise/libasciidoc/pkg/parser"
+	"github.com/bytesparadise/libasciidoc/pkg/renderer"
+	"github.com/bytesparadise/libasciidoc/pkg/renderer/html5"
+	"github.com/bytesparadise/libasciidoc/pkg/types"
+	"github.com/davecgh/go-spew/spew"
+	. "github.com/onsi/ginkgo"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = Describe("article.adoc", func() {
+
+	It("should render without failure", func() {
+		f, err := os.Open("article.adoc")
+		require.NoError(GinkgoT(), err, "Error found while opening the document")
+		reader := bufio.NewReader(f)
+		doc, err := parser.ParseReader("", reader)
+		require.NoError(GinkgoT(), err, "Error found while parsing the document")
+		GinkgoT().Logf("actual document: `%s`", spew.Sdump(doc))
+		buff := bytes.NewBuffer(nil)
+		actualDocument := doc.(types.Document)
+		rendererCtx := renderer.Wrap(context.Background(), actualDocument)
+		_, err = html5.Render(rendererCtx, buff)
+		require.NoError(GinkgoT(), err)
+
+	})
+})
