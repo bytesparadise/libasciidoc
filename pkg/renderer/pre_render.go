cff5c41156ae5b019b6f45d611a0f498ebd5fa92
Xavier Coulon <xcoulon@redhat.com>
2019-04-25 22:29:25 +0200

    remove comment
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/renderer/pre_render.go
+++ pkg/renderer/pre_render.go
@@ -18,7 +18,6 @@ func Prerender(ctx *Context) error {
 	IncludePreamble(ctx)
 	IncludeTableOfContents(ctx)
 	ProcessDocumentHeader(ctx)
-	// TODO: IncludeRevision: same logic with `Section0.Attributes[AttrRevision]`
 	if log.IsLevelEnabled(log.DebugLevel) {
 		log.Debug("pre-rendered document:")
 		spew.Dump(ctx.Document)
