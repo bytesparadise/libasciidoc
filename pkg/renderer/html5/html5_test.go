992e9d9afd84d7bd64aecd3d9a6bb02e6f512694
Xavier Coulon <xcoulon@redhat.com>
2019-09-11 22:16:57 +0200

    test(renderer): verify log output when error is logged
    same assertions as in `pkg/parser`, but in `pkg/renderer/html5`
    this time.
    
    fixes #394
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/renderer/html5/html5_test.go
+++ pkg/renderer/html5/html5_test.go
@@ -3,16 +3,20 @@ package html5_test
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"strings"
+	"os"
 
 	"github.com/bytesparadise/libasciidoc/pkg/parser"
 	"github.com/bytesparadise/libasciidoc/pkg/renderer"
 	"github.com/bytesparadise/libasciidoc/pkg/renderer/html5"
 	"github.com/davecgh/go-spew/spew"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	log "github.com/sirupsen/logrus"
 )
 
 func verify(filename, expected, content string, rendererOpts ...renderer.Option) {
@@ -37,4 +41,32 @@ func verify(filename, expected, content string, rendererOpts ...renderer.Option)
 	dmp := diffmatchpatch.New()
 	diffs := dmp.DiffMain(result, expected, true)
 	assert.Equal(GinkgoT(), expected, result, dmp.DiffPrettyText(diffs))
+}
+
+func verifyConsoleOutput(console Readable, errorMsg string) {
+	GinkgoT().Logf(console.String())
+	out := make(map[string]interface{})
+	err := json.Unmarshal(console.Bytes(), &out)
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(out["level"]).Should(Equal("error"))
+	Expect(out["msg"]).Should(Equal(errorMsg))
+}
+
+func configureLogger() (Readable, func()) {
+	fmtr := log.StandardLogger().Formatter
+
+	buf := bytes.NewBuffer(nil)
+	log.SetOutput(buf)
+	log.SetFormatter(&log.JSONFormatter{
+		DisableTimestamp: true,
+	})
+	return buf, func() {
+		log.SetOutput(os.Stdout)
+		log.SetFormatter(fmtr)
+	}
+}
+
+type Readable interface {
+	Bytes() []byte
+	String() string
 }
