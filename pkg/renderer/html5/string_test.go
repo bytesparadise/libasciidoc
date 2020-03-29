6bb6ccd39ec5a5a2d572d424feb247a5abc366fb
Xavier Coulon <xcoulon@redhat.com>
2020-03-29 16:37:56 +0200

    feat(renderer): support Copyright character replacement
    replace `(C)` with `&#169;`
    
    Fixes #524
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/renderer/html5/string_test.go
+++ pkg/renderer/html5/string_test.go
@@ -9,16 +9,23 @@ import (
 
 var _ = Describe("strings", func() {
 
-	Context("ellipsis conversion", func() {
-
-		It("text with ellipsis", func() {
-			source := `some text...`
-			// top-level section is not rendered per-say,
-			// but the section will be used to set the HTML page's <title> element
-			expected := `<div class="paragraph">
+	It("text with ellipsis", func() {
+		source := `some text...`
+		// top-level section is not rendered per-say,
+		// but the section will be used to set the HTML page's <title> element
+		expected := `<div class="paragraph">
 <p>some text&#8230;&#8203;</p>
 </div>`
-			Expect(RenderHTML(source)).To(Equal(expected))
-		})
+		Expect(RenderHTML(source)).To(Equal(expected))
+	})
+
+	It("text with copyright", func() {
+		source := `Copyright (C)`
+		// top-level section is not rendered per-say,
+		// but the section will be used to set the HTML page's <title> element
+		expected := `<div class="paragraph">
+<p>Copyright &#169;</p>
+</div>`
+		Expect(RenderHTML(source)).To(Equal(expected))
 	})
 })
