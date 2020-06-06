a4223250acea63776d5f1f0d2de323747bddcc18
Cameron Moore <moorereason@gmail.com>
2020-06-06 08:50:39 -0500

    Refactor bench and profile make targets (#589)
    Create a separate `profile` target for profiling. Only run the
    benchmarks once in this target.
    
    Update `bench` target to make five passes and save the output to a file
    for use with `benchstat`.
    
    Add a `bench-smoke` target to smoke test the top-level benchmarks by
    running them only once.  This target is run via CI to ensure that the
    benchmarks are functional.
    
    Fixes #580

--- make/bench.mk
+++ make/bench.mk
@@ -1,32 +1,20 @@
 .PHONY: bench
-## run the benchmarks on the parser
+## run the top-level benchmarks
 bench: generate-optimized
 	@mkdir -p ./tmp/bench/reports
-	@go test -cpuprofile=tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).cpu.prof \
-		-memprofile tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).mem.prof \
-		-bench=. \
-		-benchtime=100x \
+	@go test -bench=. -benchmem -count=5 -run=XXX \
 		github.com/bytesparadise/libasciidoc \
-		-run=XXX
-	@echo "generate CPU reports..."
-	@go tool pprof -text -output=tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).cpu.txt \
-		tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).cpu.prof
-ifndef CI
-	@go tool pprof -svg -output=tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).cpu.svg \
-		tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).cpu.prof
-endif
-	@echo "generate memory reports"
-	@go tool pprof -text -output=tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).mem.txt \
-		tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).mem.prof
-ifndef CI
-	@go tool pprof -svg -output=tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).mem.svg \
-		tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).mem.prof
-endif
+		| tee tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench
+	@echo "generated tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench"
 
-
+.PHONY: bench-smoke
+## smoke test the top-level benchmarks
+bench-smoke: generate-optimized
+	@go test -bench=. -benchmem -benchtime=1x -run=XXX \
+		github.com/bytesparadise/libasciidoc
 
 .PHONY: bench-parser
-## run the benchmarks on the parser
+## run the benchmarks on the parser
 bench-parser: generate
 	@ginkgo -tags bench -focus "real-world doc-based benchmarks" -memprofile=./tmp/bench/bench.memory pkg/parser
 	@ginkgo -tags bench -focus "basic stats" pkg/parser
