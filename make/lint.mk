49d610cb5be8ff4fbd7271c24644ef1f982efbe3
Xavier Coulon <xcoulon@redhat.com>
2021-03-02 22:58:09 +0100

    chore(build): use latest version of golangci-lint
    applies to GH Workflow (using the golangci-lint-action) and
    from CLI via `make lint`
    
    Fixes #833
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/lint.mk
+++ make/lint.mk
@@ -1,7 +1,7 @@
 .PHONY: install-golangci-lint
 ## Install development tools.
 install-golangci-lint:
-	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(shell go env GOPATH)/bin v1.29.0
+	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(shell go env GOPATH)/bin
 
 .PHONY: lint
 ## run golangci-lint against project
