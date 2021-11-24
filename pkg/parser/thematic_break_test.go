0b7289190229b85167ce556007cd7dc87f48592a
Xavier Coulon <xcoulon@redhat.com>
2021-11-24 23:39:57 +0100

    feat(parser): support Asciidoc thematic break (#874)
    All Markdown flavours were already supported, but not the
    Asciidoc one! (:facepalm:)
    
    Fixes #848
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/parser/thematic_break_test.go
+++ pkg/parser/thematic_break_test.go
@@ -8,111 +8,173 @@ import (
 	. "github.com/onsi/gomega" //nolint golint
 )
 
-var _ = Describe("paragraphs", func() {
+var _ = Describe("thematic breaks", func() {
 
 	Context("in final documents", func() {
 
-		Context("thematic breaks", func() {
+		It("by itself", func() {
+			source := "'''"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("form1 by itself", func() {
-				source := "***"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.ThematicBreak{},
-					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+		It("mk form1 by itself", func() {
+			source := "***"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("form2 by itself", func() {
-				source := "* * *"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.ThematicBreak{},
-					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+		It("mk form2 by itself", func() {
+			source := "* * *"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("form3 by itself", func() {
-				source := "---"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.ThematicBreak{},
-					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+		It("mk form3 by itself", func() {
+			source := "---"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("form4 by itself", func() {
-				source := "- - -"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.ThematicBreak{},
-					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+		It("mk form4 by itself", func() {
+			source := "- - -"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("form5 by itself", func() {
-				source := "___"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.ThematicBreak{},
-					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+		It("mk form5 by itself", func() {
+			source := "___"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("form4 by itself", func() {
-				source := "_ _ _"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.ThematicBreak{},
-					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+		It("mk form4 by itself", func() {
+			source := "_ _ _"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			It("with leading text", func() {
-				source := "text ***"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.Paragraph{
-							Elements: []interface{}{
-								&types.StringElement{Content: "text ***"},
-							},
+		It("with leading text", func() {
+			source := "text ***"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.Paragraph{
+						Elements: []interface{}{
+							&types.StringElement{Content: "text ***"},
 						},
 					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
 
-			// NB: three asterisks gets confused with bullets if with trailing text
-			It("with trailing text", func() {
-				source := "* * * text"
-				expected := &types.Document{
-					Elements: []interface{}{
-						&types.List{
-							Kind: types.UnorderedListKind,
-							Elements: []types.ListElement{
-								&types.UnorderedListElement{
-									BulletStyle: types.OneAsterisk,
-									CheckStyle:  types.NoCheck,
-									Elements: []interface{}{
-										&types.Paragraph{
-											Elements: []interface{}{
-												&types.StringElement{Content: "* * text"},
-											},
+		It("with trailing spaces", func() {
+			source := "'''   "
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.ThematicBreak{},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
+
+		// NB: three asterisks gets confused with bullets if with trailing text
+		It("with trailing text", func() {
+			source := "* * * text"
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.List{
+						Kind: types.UnorderedListKind,
+						Elements: []types.ListElement{
+							&types.UnorderedListElement{
+								BulletStyle: types.OneAsterisk,
+								CheckStyle:  types.NoCheck,
+								Elements: []interface{}{
+									&types.Paragraph{
+										Elements: []interface{}{
+											&types.StringElement{Content: "* * text"},
 										},
 									},
 								},
 							},
 						},
 					},
-				}
-				Expect(ParseDocument(source)).To(MatchDocument(expected))
-			})
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
 		})
+
+		// does not break when embedded within a paragrap
+		It("between 2 paragraphs", func() {
+			source := `
+some
+
+'''
+
+content`
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.Paragraph{
+						Elements: []interface{}{
+							&types.StringElement{Content: "some"},
+						},
+					},
+					&types.ThematicBreak{},
+					&types.Paragraph{
+						Elements: []interface{}{
+							&types.StringElement{Content: "content"},
+						},
+					},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
+
+		// does not break when embedded within a paragrap
+		It("within paragraph", func() {
+			source := `
+some
+'''
+content`
+			expected := &types.Document{
+				Elements: []interface{}{
+					&types.Paragraph{
+						Elements: []interface{}{
+							&types.StringElement{Content: "some\n'''\ncontent"},
+						},
+					},
+				},
+			}
+			Expect(ParseDocument(source)).To(MatchDocument(expected))
+		})
+
 	})
 })
---
+++ pkg/renderer/sgml/html5/thematic_break_test.go
@@ -0,0 +1,28 @@
+package html5_test
+
+import (
+	. "github.com/bytesparadise/libasciidoc/testsupport"
+
+	. "github.com/onsi/ginkgo" //nolint golint
+	. "github.com/onsi/gomega" //nolint golint
+)
+
+var _ = Describe("thenatic breaks", func() {
+
+	It("simple", func() {
+		source := `before
+
+'''
+
+after`
+		expected := `<div class="paragraph">
+<p>before</p>
+</div>
+<hr>
+<div class="paragraph">
+<p>after</p>
+</div>
+`
+		Expect(RenderHTML(source)).To(MatchHTML(expected))
+	})
+})
