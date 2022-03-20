13cc003cd4259ce182a606e6529b299c348ed3b7
Xavier Coulon <xcoulon@redhat.com>
2022-03-20 16:54:58 +0100

    refactor(bench): measure on demo.adoc and other docs (#985)
    bench on compatibility `demo.adoc` doc and 2 other 'real world' docs,
    which should cover more features and help measure perf improvements
    (or not) in the library
    
    Also, remove `bench-smoke` target (will be replaced later)
    
    Fixes #977

--- make/bench.mk
+++ make/bench.mk
@@ -2,21 +2,7 @@
 ## run the top-level benchmarks
 bench: clean generate-optimized
 	@mkdir -p ./tmp/bench/reports
-	@go test github.com/bytesparadise/libasciidoc -run TestParseBasicDocument
-	@go test -bench=. -benchmem -count=10 -run=XXX \
+	@go test -tags bench -bench=. -benchmem -count=10 -run=XXX \
 		github.com/bytesparadise/libasciidoc \
 		| tee tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench
 	@echo "generated tmp/bench/reports/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench"
-
-.PHONY: bench-smoke
-## smoke test the top-level benchmarks
-bench-smoke: generate-optimized
-	@go test -bench=. -benchmem -benchtime=1x -run=XXX \
-		github.com/bytesparadise/libasciidoc
-
-# .PHONY: bench-parser
-# ## run the benchmarks on the parser
-# bench-parser: generate
-# 	@ginkgo -tags bench -focus "real-world doc-based benchmarks" -memprofile=./tmp/bench/bench.memory pkg/parser
-# 	@ginkgo -tags bench -focus "basic stats" pkg/parser
-
