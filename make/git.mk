fc6b1aee1ee5392fc27eef46c9cb1eed8b22950b
Xavier Coulon <xcoulon@redhat.com>
2022-04-03 18:25:36 +0200

    symbolic ref or current commit
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/git.mk
+++ make/git.mk
@@ -3,6 +3,6 @@ ifneq ($(shell git status --porcelain),)
        GIT_COMMIT_ID_SHORT := $(GIT_COMMIT_ID_SHORT)-dirty
 endif
 
-REF_NAME := $(shell git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
+REF_NAME := $(shell git symbolic-ref -q --short HEAD || git log -1 --format='%h')
 
 BUILD_TIME = `date -u '+%Y-%m-%dT%H:%M:%SZ'`q
