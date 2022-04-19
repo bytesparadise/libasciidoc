10f801990521e30e7add033500bad744d36d136c
Xavier Coulon <xcoulon@redhat.com>
2022-04-19 22:41:39 +0200

    improve PR comment
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/bench.mk
+++ make/bench.mk
@@ -3,7 +3,6 @@ BENCH_COUNT ?= 10
 
 # Detecting GOPATH and removing trailing "/" if any
 GOPATH = $(realpath $(shell go env GOPATH))
-$(info GOPATH: ${GOPATH})
 
 .PHONY: bench
 ## run the top-level benchmarks
@@ -35,15 +34,16 @@ bench-diff: clean generate-optimized check-git-status
 	@echo "generated $(REPORTS_DIR)/v0.7.0.bench"
 	@git checkout $(GITHUB_SHA)
 	@echo "HEAD is now at $(shell git log -1 --format='%h') (expecting $(GITHUB_SHA))"
-	@echo "Comparing with 'master' branch" > $(REPORTS_DIR)/diffs.txt
-	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/master.bench >> $(REPORTS_DIR)/diffs.txt
-	@echo "" >> $(REPORTS_DIR)/diffs.txt
-	@echo "Comparing with 'v0.7.0' tag" >> $(REPORTS_DIR)/diffs.txt
-	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/v0.7.0.bench >> $(REPORTS_DIR)/diffs.txt
+	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/master.bench >> $(REPORTS_DIR)/diffs-master.txt
+	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/v0.7.0.bench >> $(REPORTS_DIR)/diffs-latest-release.txt
 
-.PHONY: print-bench-diff
-print-bench-diff:
-	@cat $(REPORTS_DIR)/diffs.txt
+.PHONY: print-bench-diff-master
+print-bench-diff-master:
+	@cat $(REPORTS_DIR)/diffs-master.txt
+
+.PHONY: print-bench-diff-latest-release
+print-bench-diff-latest-release:
+	@cat $(REPORTS_DIR)/diffs-latest-release.txt
 	
 check-git-status:
 ifneq ("$(shell git status --porcelain)","")
