8d0abeca41d99552b2c98be4b98685295ad64325
Xavier Coulon <xcoulon@redhat.com>
2022-04-19 23:07:51 +0200

    chore(bench): display bench stats as a comment in the PR (#995)
    Triggering the workflow on a `pull_request_target` event to allow for writing comments on the PR even when it comes from a fork.
    
    Fixes #992
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/bench.mk
+++ make/bench.mk
@@ -3,7 +3,6 @@ BENCH_COUNT ?= 10
 
 # Detecting GOPATH and removing trailing "/" if any
 GOPATH = $(realpath $(shell go env GOPATH))
-$(info GOPATH: ${GOPATH})
 
 .PHONY: bench
 ## run the top-level benchmarks
@@ -11,17 +10,18 @@ bench: clean generate-optimized
 	@mkdir -p $(REPORTS_DIR)
 	@go test -tags bench -bench=. -benchmem -count=$(BENCH_COUNT) -run=XXX \
 		github.com/bytesparadise/libasciidoc \
-		| tee $(REPORTS_DIR)/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench
-	@echo "generated $(REPORTS_DIR)/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench"
+		| tee $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench
+	@echo "generated $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench"
 
 .PHONY: bench-diff
 ## run the top-level benchmarks and compares with results of 'master' and 'v0.7.0'
 bench-diff: clean generate-optimized check-git-status
+	@git config advice.detachedHead false
 	@mkdir -p $(REPORTS_DIR)
 	@go test -tags bench -bench=. -benchmem -count=$(BENCH_COUNT) -run=XXX \
 		github.com/bytesparadise/libasciidoc \
-		| tee $(REPORTS_DIR)/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench
-	@echo "generated $(REPORTS_DIR)/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench"
+		| tee $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench
+	@echo "generated $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench"
 	@git checkout master
 	@go test -tags bench -bench=. -benchmem -count=$(BENCH_COUNT) -run=XXX \
 		github.com/bytesparadise/libasciidoc \
@@ -32,14 +32,19 @@ bench-diff: clean generate-optimized check-git-status
 		github.com/bytesparadise/libasciidoc \
 		| tee $(REPORTS_DIR)/v0.7.0.bench
 	@echo "generated $(REPORTS_DIR)/v0.7.0.bench"
-	@git checkout $(GIT_BRANCH_NAME)
-	@echo ""
-	@echo "Comparing with 'master' branch"
-	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/master.bench
-	@echo ""
-	@echo "Comparing with 'v0.7.0' tag"
-	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GIT_BRANCH_NAME)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/v0.7.0.bench
+	@git checkout $(GITHUB_SHA)
+	@echo "HEAD is now at $(shell git log -1 --format='%h') (expecting $(GITHUB_SHA))"
+	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/master.bench >> $(REPORTS_DIR)/diffs-master.txt
+	@$(GOPATH)/bin/benchstat $(REPORTS_DIR)/$(GITHUB_SHA)-$(GIT_COMMIT_ID_SHORT).bench $(REPORTS_DIR)/v0.7.0.bench >> $(REPORTS_DIR)/diffs-latest-release.txt
 
+.PHONY: print-bench-diff-master
+print-bench-diff-master:
+	@cat $(REPORTS_DIR)/diffs-master.txt
+
+.PHONY: print-bench-diff-latest-release
+print-bench-diff-latest-release:
+	@cat $(REPORTS_DIR)/diffs-latest-release.txt
+	
 check-git-status:
 ifneq ("$(shell git status --porcelain)","")
 	@echo "Repository contains uncommitted changes:"
--- make/git.mk
+++ make/git.mk
@@ -3,6 +3,8 @@ ifneq ($(shell git status --porcelain),)
        GIT_COMMIT_ID_SHORT := $(GIT_COMMIT_ID_SHORT)-dirty
 endif
 
-GIT_BRANCH_NAME := $(shell git rev-parse --abbrev-ref HEAD)
+# using the env var defined by GitHub Actions 
+# (see https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables)
+GITHUB_SHA ?= $(shell git rev-parse --abbrev-ref HEAD)
 
 BUILD_TIME = `date -u '+%Y-%m-%dT%H:%M:%SZ'`
