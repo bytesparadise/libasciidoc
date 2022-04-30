b70566db05ee658db10cd0ac46cda30fe61aba71
Xavier Coulon <xcoulon@redhat.com>
2022-04-30 08:41:55 +0200

    chore(project): remove usage of gover
    Fixes #1000
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/test.mk
+++ make/test.mk
@@ -4,11 +4,6 @@ install-ginkgo:
 	@go install -v github.com/onsi/ginkgo/v2/ginkgo
 	@ginkgo version
 
-.PHONY: install-gover
-## Install development tools.
-install-gover:
-	@go install -v github.com/sozorogami/gover
-
 .PHONY: test
 ## run all tests excluding fixtures and vendored packages
 test: clean generate-optimized install-ginkgo
@@ -18,10 +13,9 @@ COVERPKGS := $(shell go list ./... | grep -v vendor | paste -sd "," -)
 
 .PHONY: test-with-coverage
 ## run all tests excluding fixtures and vendored packages
-test-with-coverage: generate-optimized install-ginkgo install-gover
+test-with-coverage: generate-optimized install-ginkgo
 	@echo $(COVERPKGS)
 	@ginkgo -r --randomize-all --randomize-suites  --trace --race --compilers=0  --cover -coverpkg $(COVERPKGS)
-	@gover . coverage.txt
 
 .PHONY: test-fixtures
 ## run all fixtures tests
