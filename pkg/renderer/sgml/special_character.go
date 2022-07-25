7ee6335f5e0238a335e092897749abf05225b368
Xavier Coulon <xcoulon@redhat.com>
2022-07-25 22:20:01 +0200

    refactor(renderer): simplify rendering of special characters
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/renderer/sgml/special_character.go
+++ pkg/renderer/sgml/special_character.go
@@ -1,10 +1,20 @@
 package sgml
 
 import (
+	"fmt"
+
 	"github.com/bytesparadise/libasciidoc/pkg/types"
 )
 
 func (r *sgmlRenderer) renderSpecialCharacter(s *types.SpecialCharacter) (string, error) {
-	// log.Debugf("rendering special character '%s'", s.Name)
-	return escapeString(s.Name), nil
+	switch s.Name {
+	case `&`:
+		return "&amp;", nil
+	case `<`:
+		return "&lt;", nil
+	case `>`:
+		return "&gt;", nil
+	default:
+		return "", fmt.Errorf("unknown special character: '%s'", s.Name)
+	}
 }
